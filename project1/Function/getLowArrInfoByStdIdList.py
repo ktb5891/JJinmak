9d51d232cc46516d2399ed39fe889d2203ced27b
ktb5891 <68469395+ktb5891@users.noreply.github.com>
2020-11-06 16:41:39 +0900

    Update getLowArrInfoByStdIdList.py

--- project1/Function/getLowArrInfoByStdIdList.py
+++ project1/Function/getLowArrInfoByStdIdList.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 
 def getLastTm_By_station(stdId,search_Bus):
     serviceKey_list=[
-        "891h1XE5tlr8DWq1zPhPbyJQVZTbdU4b90ZLnIk9KLXSpk%2Bz9PKQB5PeoVGKlxd%2BARmP%2FRJM%2B4Ry6CV71XYvng%3D%3D",
-        "jqFn5RYKJTSM3Q%2FP7bw44SIu5eQ8%2FOp8bzzmVuVs0FjVkGWUYblKkIeMpbxXsOropCS%2B4ETKE0F0%2F6zR8BvPEw%3D%3D",
-        "PLLx23%2Beq0f7G2OdoALWYv1GrgOu%2B25clMxxWSInwiNssM7ennJ%2BY1KN6J%2F6osvdnDt6IADhSfBRcqMR0YmLog%3D%3D"
+        "----------------------------------------------------",
+        "----------------------------------------------------",
+        "----------------------------------------------------"
 
     ]
     serviceKey = serviceKey_list[1]
